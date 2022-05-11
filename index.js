0adbd258b828745c74ea4cd155c3a937d01f386d
aminhuang <aminhuang@tencent.com>
2022-05-11 17:38:07 +0800

    update console

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.get("/", async (req, res) => {
 
 // 更新计数
 app.post("/api/count", async (req, res) => {
-  console.log('/api/count', req.body)
+  console.log('/api/count req body:', req.body)
+  console.log('/api/count req body action:', req.body.action)
   const { action } = req.body;
   if (action === "inc") {
     await Counter.create();
